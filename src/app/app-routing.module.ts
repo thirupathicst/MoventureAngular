f72510beb8ed90b4804f0fb9a6a0a3f108d9aff8
thirupathi <athirupathi@delaplex.com>
2022-01-12 13:08:04 +0530

    Bootstrap5.1 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ActorComponent } from './actor/actor.component';
+import { GenersComponent } from './geners/geners.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { MovieComponent } from './movie/movie.component';
+import { WatchlistComponent } from './watchlist/watchlist.component';
+import { AuthorizedlayoutComponent } from './authorizedlayout/authorizedlayout.component';
+import { GuestlayoutComponent } from './guestlayout/guestlayout.component';
+import { MoviedetailsComponent } from './moviedetails/moviedetails.component';
+
+const routes: Routes = [
+  {
+    path: '', component: AuthorizedlayoutComponent, children: [
+      { path: 'actor', component: ActorComponent },
+      { path: 'movie', component: MovieComponent },
+      { path: 'watchlist', component: WatchlistComponent },
+      { path: 'genres', component: GenersComponent },
+      {path:'moviedetails',component:MoviedetailsComponent}
+    ]
+  },
+  {
+    path: '', component: GuestlayoutComponent, children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'signup', component: SignupComponent },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
